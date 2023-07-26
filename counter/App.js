c3a510e31a9d2a22a97f13ad5309bddb0370a05b
parkdaxun <parkde0207@gmail.com>
2023-07-26 16:09:56 +0900

    Counter 만들기

--- counter/App.js
+++ counter/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Counter from "./components/Counter";
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  full: {
+    flex:1,
+    backgroundColor:'lightblue',
   },
 });
+
+export default App;
