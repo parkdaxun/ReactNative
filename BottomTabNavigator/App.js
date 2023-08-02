5254c67a52eaca35b446265b95639d50f4692091
parkdaxun <parkde0207@gmail.com>
2023-08-02 15:39:39 +0900

    BottomTabBarNavigation에 이름, 색 추가하기

--- BottomTabNavigator/App.js
+++ BottomTabNavigator/App.js
@@ -2,6 +2,7 @@ import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {SafeAreaView} from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
 
 const Tab = createBottomTabNavigator();
@@ -25,11 +26,52 @@ function MessageScreen() {
 function App() {
   return (
       <NavigationContainer>
-        <Tab.Navigator initialRouteName="Home" >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="Notification" component={NotificationScreen} />
-          <Tab.Screen name="Message" component={MessageScreen} />
+        <Tab.Navigator
+            initialRouteName="Home"
+            tabBarOptions={{
+                activeTintColor: "#3e6533",
+                showLabel: true,
+            }}>
+          <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                  title: '홈',
+                  tabBarIcon: ({color, size}) => (
+                      <Icon name="home" color={color} size={size} />
+                  ),
+              }}
+          />
+          <Tab.Screen
+              name="Search"
+              component={SearchScreen}
+              options={{
+                  title: '검색',
+                  tabBarIcon: ({color, size}) => (
+                      <Icon name="search" color={color} size={size} />
+                  ),
+              }}
+          />
+          <Tab.Screen
+              name="Notification"
+              component={NotificationScreen}
+              options={{
+                  title: '알림',
+                  tabBarIcon: ({color, size}) => (
+                      <Icon name="notifications" color={color} size={size} />
+                  ),
+              }}
+          />
+          <Tab.Screen
+              name="Message"
+              component={MessageScreen}
+              options={{
+                  title: '메세지',
+                  tabBarIcon: ({color, size}) => (
+                      <Icon name="message" color={color} size={size} />
+                  ),
+              }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
   );
