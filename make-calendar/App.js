590b97db18be50fdccfbabe1f0a11282cf0c227f
parkdaxun <parkde0207@gmail.com>
2023-08-08 15:55:15 +0900

    [ADD] 달력에 표시하기

--- make-calendar/App.js
+++ make-calendar/App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {Calendar} from 'react-native-calendars';
 
 export default function App() {
+  const markedDates = {
+    '2023-08-08': {
+      selected: true,
+    },
+
+    '2023-08-13': {
+      marked: true,
+    },
+  }
+
   return (
       <View>
         <View style={styles.box} />
-        <Calendar style={styles.calendar} />
+        <Calendar
+            style={styles.calendar}
+            markedDates={markedDates}
+            theme={{
+              selectedDayBackgroundColor: '#009688',
+              arrowColor: '#009688',
+              dotColor: '#009688',
+              todayTextColor: '#009688',
+            }}
+        />
       </View>
   );
 }
