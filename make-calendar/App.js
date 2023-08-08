c610fe2f25a0bc7d6830977cbd739c4e438d7008
parkdaxun <parkde0207@gmail.com>
2023-08-08 15:49:22 +0900

    [ADD] calendar

--- make-calendar/App.js
+++ make-calendar/App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Calendar} from 'react-native-calendars';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <View style={styles.box} />
+        <Calendar style={styles.calendar} />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  box: {
+    height : 50,
+  },
+  calendar: {
+    borderBottomColor: '#e0e0e0',
   },
 });
