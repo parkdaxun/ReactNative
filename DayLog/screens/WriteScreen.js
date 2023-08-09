f23126be61818a43715eee256c9475da47d924cc
parkdaxun <parkde0207@gmail.com>
2023-08-09 17:08:13 +0900

    [ADD] WriteEditor Screen

--- DayLog/screens/WriteScreen.js
+++ DayLog/screens/WriteScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from "react-native";
 import WriteHeader from "../component/WriteHeader";
+import WriteEditor from "../component/WriteEditor";
 
 function WriteScreen() {
     return (
         <SafeAreaView style={styles.block}>
             <WriteHeader />
+            <WriteEditor />
         </SafeAreaView>
     );
 }
