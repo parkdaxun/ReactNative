d8450754f4f1067d4a56c9aa96853f486a6e87e6
parkdaxun <parkde0207@gmail.com>
2023-08-09 17:36:12 +0900

    [FIX] WriteScreen에서 텍스트 상태관리

--- DayLog/screens/WriteScreen.js
+++ DayLog/screens/WriteScreen.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from "react-native";
+import React, {useState} from 'react';
+import {KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet} from "react-native";
 import WriteHeader from "../component/WriteHeader";
 import WriteEditor from "../component/WriteEditor";
 
 function WriteScreen() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
     return (
         <SafeAreaView style={styles.block}>
-            <WriteHeader />
-            <WriteEditor />
+            <KeyboardAvoidingView
+                style={styles.avoidingView}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+                <WriteHeader />
+                <WriteEditor
+                    title={title}
+                    body={body}
+                    onChangeTitle={setTitle}
+                    onChangeBody={setBody}
+                />
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
@@ -16,6 +27,10 @@ const styles = StyleSheet.create({
     block: {
         flex : 1,
         backgroundColor : 'white',
+    },
+
+    avoidingView : {
+        flex : 1,
     },
 });
 
