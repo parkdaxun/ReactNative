7d3c7e15fcdba127187b790a8cb1cb678204eebc
parkdaxun <parkde0207@gmail.com>
2023-08-10 14:46:49 +0900

    [ADD] FeedListItem

---
+++ DayLog/component/FeedLisItem.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Platform, Pressable, StyleSheet, Text} from 'react-native';
+
+function truncate(text) {
+    // 줄 바꿈 문자 제거하기
+    const replaced = text.replace(/\n/g, '');
+    if(replaced.length <= 100) {
+        return replaced;
+    }
+    return replaced.slice(0, 100).concat('...');
+}
+
+function FeedListItem({log}) {
+    const {title, body, date} = log;
+
+    return(
+        <Pressable
+            style={({pressed}) => [
+                styles.block,
+                Platform.OS === 'ios' && pressed && {backgroundColor : '#efefef'},
+            ]}>
+            <Text style={styles.date}>{new Date(date).toLocalString()}</Text>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.body}>{body}</Text>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    block : {
+        backgroundColor : 'white',
+        paddingHorizontal : 16,
+        paddingVertical : 24,
+    },
+
+    date : {
+        fontSize : 12,
+        color : '#546e7a',
+    },
+
+    title : {
+        color : '#263238',
+        fontSize : 18,
+        fontWeight : 'bold',
+        marginBottom : 8,
+    },
+
+    body : {
+        color : '#37474f',
+        fontSize : 16,
+        lineHeight : 21,
+    },
+});
+
+export default FeedListItem;
