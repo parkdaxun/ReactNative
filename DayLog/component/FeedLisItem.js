1e842767264d7d55336f5ba997cd3027892c1804
parkdaxun <parkde0207@gmail.com>
2023-08-10 15:30:44 +0900

    [FEAT] date-fns로 날짜 포맷팅하기

--- DayLog/component/FeedLisItem.js
+++ DayLog/component/FeedLisItem.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import {Platform, Pressable, StyleSheet, Text} from 'react-native';
+import {format, formatDistanceToNow} from 'date-fns';
+import {ko} from 'date-fns/locale';
+
+function formatDate(date) {
+    const d = new Date(date);
+    const now = Date.now();
+    const diff = (now -d.getTime()) / 1000;
+
+    if(diff < 60 * 1) {
+        return '방금 전';
+    }
+    if(diff < 60 * 60 * 24 * 3) {
+        return formatDistanceToNow(d, {addSuffix: true, locale: ko});
+    }
+    return format(d, 'PPP EEE p', {locale: ko});
+}
 
 function truncate(text) {
     // 줄 바꿈 문자 제거하기
@@ -19,7 +35,7 @@ function FeedListItem({log}) {
                 styles.block,
                 Platform.OS === 'ios' && pressed && {backgroundColor : '#efefef'},
             ]}>
-            <Text style={styles.date}>{new Date(date).toLocaleString()}</Text>
+            <Text style={styles.date}>{formatDate(date)}</Text>
             <Text style={styles.title}>{title}</Text>
             <Text style={styles.body}>{body}</Text>
         </Pressable>
