b86464d13a4f5f14cbdce616422a646eab783b94
parkdaxun <parkde0207@gmail.com>
2023-08-16 08:21:26 +0900

    [FEAT] 달력에 표시하기

--- DayLog/component/CalendarView.js
+++ DayLog/component/CalendarView.js
@@ -3,8 +3,32 @@ import {Calendar} from 'react-native-calendars';
 import {StyleSheet} from "react-native";
 
 function CalendarView() {
+
+    const markedDates = {
+        '2023-08-14' : {
+            selected: true,
+        },
+
+        '2023-08-03' : {
+            selected: true,
+        },
+
+        '2023-08-10' : {
+            selected: true,
+        },
+    };
+
     return (
-      <Calendar style={styles.calendar} />
+      <Calendar
+          style={styles.calendar}
+          markedDates={markedDates}
+          theme={{
+              selectedDayBackgroundColor : '#1c4811',
+              arrowColor : '#1c4811',
+              dotColor : '#1c4811',
+              todayTextColor : '#1c4811',
+          }}
+      />
     );
 }
 
