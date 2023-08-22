e452da2949e0c385fea0e291be1be00c3b0e1aff
parkdaxun <parkde0207@gmail.com>
2023-08-22 17:22:16 +0900

    [FEAT] 검색어 필터링 + FeedList

--- DayLog/DayLog/screens/SearchScreen.js
+++ DayLog/DayLog/screens/SearchScreen.js
@@ -1,19 +1,29 @@
 import React, {useContext} from 'react';
 import {StyleSheet, Text, View} from "react-native";
 import SearchContext from "../contexts/SearchContext";
+import FeedList from "../component/FeedList";
+import LogContext from "../contexts/LogContext";
 
 function SearchScreen() {
     const {keyword} = useContext(SearchContext);
+    const {logs} = useContext(LogContext);
+
+    const filtered =
+        keyword === ''
+            ? []
+            : logs.filter((log) =>
+                [log.title, log.body].some((text) => text.includes(keyword)),
+            );
 
     return (
         <View style={styles.block}>
-            <Text>{keyword}</Text>
+            <FeedList logs={filtered} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    block: {},
+    block: {flex :1},
 });
 
 export default SearchScreen;
