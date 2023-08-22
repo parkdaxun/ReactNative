4a05810c652f9620099490cd6ee8e665e836d9ce
parkdaxun <parkde0207@gmail.com>
2023-08-23 08:19:01 +0900

    [ADD] TodoListScreen

--- DayLog/DayLog/screens/MainTab.js
+++ DayLog/DayLog/screens/MainTab.js
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FeedsScreen from "./FeedsScreen";
 import CalendarScreen from "./CalendarScreen";
 import SearchScreen from "./SearchScreen";
+import TodoListScreen from "./TodoListScreen";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import SearchHeader from "../component/SearchHeader";
 
@@ -15,6 +16,15 @@ function MainTab() {
                 showLabel: false,
                 activeTintColor: '#000000',
             }}>
+            <Tab.Screen
+                name="ToDo List"
+                component={TodoListScreen}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Icon name="check" size={size} color={color} />
+                    ),
+                }}
+            />
             <Tab.Screen
                 name="Feeds"
                 component={FeedsScreen}
