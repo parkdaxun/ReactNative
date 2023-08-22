395c8fe863fd21cfb30955fded25e75d8beb40c5
parkdaxun <parkde0207@gmail.com>
2023-08-22 17:14:52 +0900

    [ADD] SearchContext

--- DayLog/DayLog/component/SearchHeader.js
+++ DayLog/DayLog/component/SearchHeader.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Pressable, TextInput, StyleSheet, View, useWindowDimensions} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import SearchContext from "../contexts/SearchContext";
 
 function SearchHeader() {
     const {width} = useWindowDimensions();
+    const {keyword, onChangeText} = useContext(SearchContext);
+
     return (
         <View style={[styles.block, {width: width-32, height: 24}]}>
-            <TextInput style={styles.input} placeholder="검색어를 입력하세요" autoFocus />
+            <TextInput style={styles.input} placeholder="검색어를 입력하세요" value={keyword} onChangeText={onChangeText} />
             <Pressable
                 style={({pressed}) => [styles.button, pressed && {opacity : 0.5}]}>
                 <Icon name="cancel" size={20} color="#9e9e9e" />
